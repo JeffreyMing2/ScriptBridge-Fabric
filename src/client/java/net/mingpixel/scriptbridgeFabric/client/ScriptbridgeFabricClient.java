3925b51824fe478073bf7cd4b54a74bb1e4446be
JeffreyMing <1640053235@qq.com>
2026-01-17 16:04:54 +0800

    feat(client): 添加打开脚本文件夹的客户端命令
    添加一个客户端命令 `/script openfile` 用于快速打开脚本文件夹

--- src/client/java/net/mingpixel/scriptbridgeFabric/client/ScriptbridgeFabricClient.java
+++ src/client/java/net/mingpixel/scriptbridgeFabric/client/ScriptbridgeFabricClient.java
@@ -1,9 +1,14 @@
 package net.mingpixel.scriptbridgeFabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Util;
+import java.io.File;
 
 public class ScriptbridgeFabricClient implements ClientModInitializer {
 
@@ -17,6 +22,25 @@ public class ScriptbridgeFabricClient implements ClientModInitializer {
                     false
                 );
             }
+        });
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("script")
+                .then(ClientCommandManager.literal("openfile")
+                    .executes(context -> {
+                        File runDir = MinecraftClient.getInstance().runDirectory;
+                        File scriptsDir = new File(runDir, "scripts");
+                        
+                        if (!scriptsDir.exists()) {
+                            scriptsDir.mkdirs();
+                        }
+
+                        Util.getOperatingSystem().open(scriptsDir);
+                        context.getSource().sendFeedback(Text.literal("正在打开脚本文件夹...").formatted(Formatting.GREEN));
+                        return 1;
+                    })
+                )
+            );
         });
     }
 }
