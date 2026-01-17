be85ae369295b767114c5ab2e5c8afe7bbf41640
JeffreyMing <1640053235@qq.com>
2026-01-17 15:43:50 +0800

    feat: 初始化Fabric模组项目基础结构
    添加基础Fabric模组项目文件结构，包括Gradle配置、源码目录、资源文件和IDE配置文件

---
+++ src/client/java/net/mingpixel/scriptbridgeFabric/client/ScriptbridgeFabricClient.java
@@ -0,0 +1,10 @@
+package net.mingpixel.scriptbridgeFabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScriptbridgeFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/mingpixel/scriptbridgeFabric/ScriptbridgeFabric.java
@@ -0,0 +1,10 @@
+package net.mingpixel.scriptbridgeFabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ScriptbridgeFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
